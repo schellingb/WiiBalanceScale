6e27702c5effd97587396402505db48622a2b8d2
Bernhard Schelling <14200249+schellingb@users.noreply.github.com>
2023-08-06 00:31:49 +0900

    Increase version number

--- WiiBalanceScale.cs
+++ WiiBalanceScale.cs
@@ -32,8 +32,8 @@ using RadioButton = System.Windows.Forms.RadioButton;
 
 [assembly: System.Reflection.AssemblyTitle("WiiBalanceScale")]
 [assembly: System.Reflection.AssemblyProduct("WiiBalanceScale")]
-[assembly: System.Reflection.AssemblyVersion("1.1.0.0")]
-[assembly: System.Reflection.AssemblyFileVersion("1.1.0.0")]
+[assembly: System.Reflection.AssemblyVersion("1.2.0.0")]
+[assembly: System.Reflection.AssemblyFileVersion("1.2.0.0")]
 [assembly: System.Runtime.InteropServices.ComVisible(false)]
 
 namespace WiiBalanceScale
